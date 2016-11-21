b0f98a6f693d876a3c1792b306183c4a40912698
Sujan Basu <sujan1basu@gmail.com>
2016-11-21 15:51:24 +0000

    editing and updating idea

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -8,8 +8,21 @@ class IdeasController < ApplicationController
         redirect_to root_path
     end
     
+    def edit
+        @idea = Idea.find(params[:id])
+    end
+    
+    def update
+        @idea = Idea.find(params[:id])
+       if @idea.update(idea_params)
+           redirect_to root_path
+    else
+        redirect_to edit_idea_path(parmas[:id])
+    end
+    
     def idea_params
         params.require(:idea).permit(:description, :author)
      end
+ end
  end
 
