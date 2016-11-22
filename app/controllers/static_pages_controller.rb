4354234a128cb9aceae62762b96aab141017d042
Sujan Basu <sujan1basu@gmail.com>
2016-11-22 07:28:16 +0000

    added routes for about and random

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+    def about
+    end
+
+    def random
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
  root "ideas#index"
  
  resources :ideas
+ get 'about' => 'static_pages#about'
+ get 'random' => 'static_pages#random'
 end
